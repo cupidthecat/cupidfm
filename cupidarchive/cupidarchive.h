e9140801dda15f3d4f0ce7277b7c2a36d0386249
Frank <101807917+frankischilling@users.noreply.github.com>
2026-01-24 20:28:49 -0500

    Add main header for CupidArchive library

---
+++ cupidarchive/cupidarchive.h
@@ -0,0 +1,14 @@
+#ifndef CUPIDARCHIVE_H
+#define CUPIDARCHIVE_H
+
+/**
+ * Main header for CupidArchive library.
+ * Include this to use the library.
+ */
+
+#include "src/arc_reader.h"
+#include "src/arc_stream.h"
+#include "src/arc_filter.h"
+
+#endif // CUPIDARCHIVE_H
+
